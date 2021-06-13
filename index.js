850e06a35135673b8b5497574af2a070c68f7002
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-13 18:11:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function BoardMember(name,homeState,training) {
+this.name =name;
+this.homeState=homeState;
+this.training=training;
+this.veto = function()  {
+    console.log("No, I must disagree");
+  };
+}
+
+  
+}
