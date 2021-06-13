f6a2a808903b0d4bd0e3a9bfb49306691fc52c22
Yesim nur Akar <yesimnurakar@gmail.com>
2021-06-13 18:15:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ this.releasePressStatement = function() {
   return "You will see great things from Scuber."
 }
 this.sayHi = function() {
-  return ""Hi, my name is <name>. I am from <homeState>, and I was trained in <training>."
+  return "Hi, my name is <name>. I am from <homeState>, and I was trained in <training>."
 }
 }
